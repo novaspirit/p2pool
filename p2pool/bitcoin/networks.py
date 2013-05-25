cb91b32723acbca7a747129754d8a9e608c2c9e2
novaspirit <x3maniac@gmail.com>
2013-05-24 22:32:22 -0300

    added support for digital coin

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -181,6 +181,25 @@ nets = dict(
         SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
         DUMB_SCRYPT_DIFF=2**16,
     ),
+    digital=math.Object(
+        P2P_PREFIX='fbc0b6db'.decode('hex'),
+        P2P_PORT=7999,
+        ADDRESS_VERSION=30,
+        RPC_PORT=7998,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'digitalcoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 1*10000000 >> (height + 1)//1080000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=30, # s targetspacing
+        SYMBOL='DGC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'digitalcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/digitalcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.digitalcoin'), 'digitalcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://dgc.p2pool.nl/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://dgc.p2pool.nl/address/',
+        SANE_TARGET_RANGE=(2**256//100000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+    ),
 
 )
 for net_name, net in nets.iteritems():
