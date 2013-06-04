9586c078f8a20be5c3ce9cf8ecd75ea671b246b9
novaspirit <x3maniac@gmail.com>
2013-06-04 16:24:12 -0400

    updated network.py to support worldcoin

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -200,6 +200,25 @@ nets = dict(
         SANE_TARGET_RANGE=(2**256//100000000 - 1, 2**256//1000 - 1),
         DUMB_SCRYPT_DIFF=2**16,
     ),
+    worldcoin=math.Object(
+        P2P_PREFIX='fbc0b6db'.decode('hex'),
+        P2P_PORT=11081,
+        ADDRESS_VERSION=73,
+        RPC_PORT=11082,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'worldcoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 32*100000000 >> (height + 1)//2650000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=15,
+        SYMBOL='WDC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Worldcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Worldcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.worldcoin'), 'worldcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://wdc.a-df.net:2750/chain/Worldcoin/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://wdc.a-df.net:2750/chain/Worldcoin/address/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+    ),
 
 )
 for net_name, net in nets.iteritems():
